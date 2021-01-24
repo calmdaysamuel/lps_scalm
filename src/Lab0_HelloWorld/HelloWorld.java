0f0a713744008e5b0e9571a53c70b1e8151e955b
Samuel Calmday <calmdaysamuel@gmail.com>
2021-01-24 14:43:05 -0800

    Lab 0: Hello World

--- src/Lab0_HelloWorld/HelloWorld.java
+++ src/Lab0_HelloWorld/HelloWorld.java
@@ -2,6 +2,6 @@ package Lab0_HelloWorld;
 
 public class HelloWorld {
     public static void main(String [] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello World");
     }
 }
